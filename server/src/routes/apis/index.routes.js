2a9ddf97ff72aec9b5890155491a7f7afccb44b9
Phu Vo <u1153426@umail.usq.edu.au>
2026-08-09 23:52:49 +1000

    :lock: feat: Add authentication and role authorization middleware

--- server/src/routes/apis/index.routes.js
+++ server/src/routes/apis/index.routes.js
@@ -4,6 +4,7 @@ import customerRoutes from './customer.routes.js'
 import productRoutes from './product.routes.js'
 import registrationRoutes from './registration.routes.js'
 import technicianRoutes from './technician.routes.js'
+import profileRoutes from './profile.routes.js'
 
 const router = Router()
 
@@ -12,5 +13,6 @@ router.use('/customers', customerRoutes)
 router.use('/products', productRoutes)
 router.use('/registrations', registrationRoutes)
 router.use('/technicians', technicianRoutes)
+router.use('/profile', profileRoutes)
 
 export default router
---
+++ server/src/services/profile.service.js
@@ -0,0 +1,98 @@
+import { Administrator, Country, Customer, Technician, User } from '../models/index.js'
+import { NotFoundError } from '../utils/app-error.js'
+
+const toPublicProfile = (user) => {
+  const baseProfile = {
+    userId: user.userId,
+    email: user.email,
+    role: user.role,
+    isActive: user.isActive
+  }
+
+  switch (user.role) {
+    case 'admin':
+      return {
+        ...baseProfile,
+        profile: user.administrator
+          ? {
+              username: user.administrator.username
+            }
+          : null
+      }
+
+    case 'technician':
+      return {
+        ...baseProfile,
+        profile: user.technician
+          ? {
+              techId: user.technician.techId,
+              firstName: user.technician.firstName,
+              lastName: user.technician.lastName,
+              phone: user.technician.phone
+            }
+          : null
+      }
+
+    case 'customer':
+      return {
+        ...baseProfile,
+        profile: user.customer
+          ? {
+              customerId: user.customer.customerId,
+              firstName: user.customer.firstName,
+              lastName: user.customer.lastName,
+              address: user.customer.address,
+              city: user.customer.city,
+              state: user.customer.state,
+              postalCode: user.customer.postalCode,
+              countryCode: user.customer.countryCode,
+              country: user.customer.country
+                ? {
+                    countryCode: user.customer.country.countryCode,
+                    countryName: user.customer.country.countryName
+                  }
+                : null,
+              phone: user.customer.phone
+            }
+          : null
+      }
+
+    default:
+      return baseProfile
+  }
+}
+
+const getProfileByUserId = async (userId) => {
+  const user = await User.findByPk(userId, {
+    include: [
+      {
+        model: Administrator,
+        as: 'administrator'
+      },
+      {
+        model: Technician,
+        as: 'technician'
+      },
+      {
+        model: Customer,
+        as: 'customer',
+        include: {
+          model: Country,
+          as: 'country'
+        }
+      }
+    ]
+  })
+
+  if (!user) {
+    throw new NotFoundError(`User with ID "${userId}" not found`)
+  }
+
+  return toPublicProfile(user)
+}
+
+const profileService = {
+  getProfileByUserId
+}
+
+export default profileService
