1ca0c42568f3dbbf7645034fab97a6a727a8dcdd
Phu Vo <u1153426@umail.usq.edu.au>
2026-08-10 02:26:02 +1000

    :sparkles: feat: add protected incident API workflows

--- server/src/routes/apis/index.routes.js
+++ server/src/routes/apis/index.routes.js
@@ -1,6 +1,7 @@
 import { Router } from 'express'
 import authRoutes from './auth.routes.js'
 import customerRoutes from './customer.routes.js'
+import incidentRoutes from './incident.routes.js'
 import productRoutes from './product.routes.js'
 import registrationRoutes from './registration.routes.js'
 import technicianRoutes from './technician.routes.js'
@@ -10,6 +11,7 @@ const router = Router()
 
 router.use('/auth', authRoutes)
 router.use('/customers', customerRoutes)
+router.use('/incidents', incidentRoutes)
 router.use('/products', productRoutes)
 router.use('/registrations', registrationRoutes)
 router.use('/technicians', technicianRoutes)
