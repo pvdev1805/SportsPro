8f9321d0b1918459994580e831032c2d5873d797
Phu Vo <u1153426@umail.usq.edu.au>
2026-08-10 05:47:29 +1000

    :white_check_mark: feat: validate page route parameters

--- server/src/routes/pages/technician.routes.js
+++ server/src/routes/pages/technician.routes.js
@@ -1,11 +1,17 @@
 import { Router } from 'express'
 import * as technicianController from '../../controllers/pages/technician.controller.js'
 import asyncHandler from '../../utils/async-handler.js'
+import validate from '../../middlewares/validate.middleware.js'
+import { technicianIdParamsOnlySchema } from '../../validators/technician.validator.js'
 
 const router = Router()
 
 router.get('/', asyncHandler(technicianController.renderTechnicianPage))
 router.get('/create', asyncHandler(technicianController.renderTechnicianCreatePage))
-router.get('/:technicianId/edit', asyncHandler(technicianController.renderTechnicianEditPage))
+router.get(
+  '/:technicianId/edit',
+  validate(technicianIdParamsOnlySchema),
+  asyncHandler(technicianController.renderTechnicianEditPage)
+)
 
 export default router
